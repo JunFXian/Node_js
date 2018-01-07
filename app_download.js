468f5ba3962d1648e25dee2e5ae53cc24ef2b761
Jun Xian <jxiangx@gmail.com>
2018-01-07 11:32:11 -0800

    Add files via upload

--- app_download.js
+++ app_download.js
@@ -7,9 +7,9 @@ const mysql = require('mysql');
 //create a connection to the DB
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: '***',
-    password: '***',
-    database: '***DB'
+    user: 'root',
+    password: 'mysql1234',
+    database: 'RecipesDB'
 });
 
 connection.connect(function(err) {
@@ -19,10 +19,39 @@ connection.connect(function(err) {
 });
 
 function downloadRecipesData() {
-	connection.query('SELECT * FROM recipes', function(err,rows) {
+	var recipesCols = 'recipes.id, recipes.name, recipes.servings, recipes.image';
+	var ingredientsCols = 'ingredients.quantity, ingredients.measure, ingredients.ingredient';
+	var stepsCols = 'steps.stepId, steps.shortDescription, steps.description, steps.videoURL, steps.thumbnailURL';
+	var recipesQuery = 'SELECT ' + recipesCols + ' FROM recipes';
+	var ingredientsQuery = 'SELECT ' + ingredientsCols + ' FROM ingredients';
+	var stepsQuery = 'SELECT ' + stepsCols + ' FROM steps';
+	connection.query(recipesQuery, function(err, result, fields) {
   		if(err) throw err;
-  		console.log('Data received from DB:\n');
-  		console.log(rows);
+  		console.log('Data received from DB:');
+  		Object.keys(result).forEach(function(key) {
+  			var rowData = JSON.stringify(result[key]);
+        	var jsonData =  JSON.parse(rowData);
+        	connection.query(ingredientsQuery + ' WHERE ingredients.recipeId = ' + jsonData.id,
+        		 function(err1, result1, fields1) {
+  				if(err1) throw err1;
+  				console.log('Data received from ingredients table:');
+  				Object.keys(result1).forEach(function(key1) {
+  					var rowData1 = JSON.stringify(result1[key1]);
+        			var jsonData1 =  JSON.parse(rowData1);
+        			console.log(jsonData1);
+  				});
+  			});
+  			connection.query(stepsQuery + ' WHERE steps.recipeId = ' + jsonData.id,
+        		 function(err2, result2, fields2) {
+  				if(err2) throw err2;
+  				console.log('Data received from steps table:');
+  				Object.keys(result2).forEach(function(key2) {
+  					var rowData2 = JSON.stringify(result2[key2]);
+        			var jsonData2 =  JSON.parse(rowData2);
+        			console.log(jsonData2);
+  				});
+  			});
+        });
   		endConnect();
 	});
 }
