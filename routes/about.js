46efd4585969d8c81804dbfe5f474422ab7b8b55
Jun Xian <jxiangx@gmail.com>
2018-01-16 15:23:26 -0800

    Delete about.js

--- routes/about.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.send('About: this is a project to upload recipes');
-});
-
-module.exports = router;
